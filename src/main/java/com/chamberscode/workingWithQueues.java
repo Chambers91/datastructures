03c1af17ecc4c630ec409af60f372757f1f293ac
Terence Chambers <terencechambers91@gmail.com>
2023-07-05 09:44:31 -0400

    Working with LinkedList

--- src/main/java/com/chamberscode/workingWithQueues.java
+++ src/main/java/com/chamberscode/workingWithQueues.java
@@ -1,11 +1,30 @@
 package com.chamberscode;
 
 import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.Queue;
 
 //FIFO - first in first out
 public class workingWithQueues {
     public static void main(String[] args) {
+
+        //linked list
+        LinkedList<Person> linkedList = new LinkedList<>();
+        linkedList.add(new Person("Alex", 21));
+        linkedList.add(new Person("Alexis", 31));
+        linkedList.add(new Person("Ally", 18));
+        linkedList.addFirst(new Person("Tommy", 33));
+
+        ListIterator<Person> personListIterator = linkedList.listIterator();
+        while (personListIterator.hasNext())
+            System.out.println(personListIterator.next());
+        System.out.println();
+
+        while (personListIterator.hasNext())
+            System.out.println(personListIterator.previous());
+    }
+
+    private static void queues() {
         Queue<Person> supermarket = new LinkedList<>();
         supermarket.add(new Person("Alex", 11));
         supermarket.add(new Person("John", 18));
@@ -22,7 +41,7 @@ public class workingWithQueues {
         System.out.println(supermarket.peek());
 
     }
-        static record Person(String name, int age) {
-        }
-        ;
+
+    static record Person(String name, int age) {
+    }
 }

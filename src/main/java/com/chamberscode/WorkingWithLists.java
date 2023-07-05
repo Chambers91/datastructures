6e4d4b2acb53899252fd1b749b0fec8845cc4340
Terence Chambers <terencechambers91@gmail.com>
2023-07-04 20:21:12 -0400

    Working with Lists data structures and array lists

--- src/main/java/com/chamberscode/WorkingWithLists.java
+++ src/main/java/com/chamberscode/WorkingWithLists.java
@@ -2,12 +2,22 @@ package com.chamberscode;
 
 import java.util.ArrayList;
 import java.util.List;
-
+//COLLECTION OF ELEMENTS
+//arraylist is used a lot
 public class WorkingWithLists {
      public static void main(String[] args){
+         List<String> colorsUnmodifiable = List.of(
+                 "blue",
+                 "yellow"
+         );
+
          List<String> colors = new ArrayList<>();
          colors.add("blue");
          colors.add("purple");
+         colors.add("yellow");
+         System.out.println(colors.size());
+         System.out.println(colors.contains("yellow"));
+         System.out.println(colors.contains("pink"));
 
          System.out.println(colors);
      }

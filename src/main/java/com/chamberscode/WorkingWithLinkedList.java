1a3b725a4b2c7f37963160d5666a38816c92c424
Terence Chambers <terencechambers91@gmail.com>
2023-07-06 11:12:52 -0400

    Working with linkedList

---
+++ src/main/java/com/chamberscode/WorkingWithLinkedList.java
@@ -0,0 +1,48 @@
+package com.chamberscode;
+
+public class WorkingWithLinkedList {
+
+    //Types of linked list
+    //single linked list - navigation is forward only
+    //- made up of *nodes* that consists of two parts, data and link
+    //example : Suppose we want to store a list of numbers: 11,21,19,91
+    // each number will have its own node |11|2000|  |21|3000| |19|4000| |91|null|
+    //                   node addresses:  1000       2000      3000      4000
+    //having each address connected to a different node, they are reachable
+    //first pointer should allow you to access the head pointer
+
+    // doubly linked list - forward and backward navigation is possible
+    //circular linked list - last element is linked to the first element
+
+    public static void main(String[] args){
+class Node{
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+//creating new objects
+
+        Node head = new Node(11);
+        Node nodeB = new Node(21);
+        Node nodeC = new Node(19);
+        Node nodeD = new Node(91);
+
+        head.next = nodeB;
+        nodeB.next = nodeC;
+        nodeC.next = nodeD;
+    }
+
+   public static int countNodes(Node head){
+ int count = 1;
+ Node current = head;
+ while(current.next != null){
+     current = current.next;
+     count+= 1;
+ }
+         return count;
+}}
+
+    }
+
+}
